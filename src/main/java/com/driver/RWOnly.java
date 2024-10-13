559348c8a3b1e83b4e61ab81cf0a54de93c2b83d
Andrew <Andrew.jayaraju@gmal.com>
2024-10-13 19:05:15 +0530

    Done

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    // Task 5: Implement getter and setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
